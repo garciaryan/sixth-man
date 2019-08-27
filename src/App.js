9772c9bb8730629b2bbec389bec09c81d646babf
ryan <ryan@digitalattic.com>
2019-08-27 15:46:07 -0700

    feat: adds deep linking handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Linking } from 'react-native';
 import RouterComponent from './Router';
 import { applyMiddleware, createStore } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
+import { YAHOO_API_ID, YAHOO_API_SECRET } from 'react-native-dotenv';
+// import { JSO, Popup } from 'jso';
+
+// const client = new JSO({
+// 	providerID: 'yahoo',
+// 	client_id: YAHOO_API_ID,
+// 	redirect_uri: 'com.sixthman:/callback',
+// 	authorization: `https://api.login.yahoo.com/oauth2/`,
+// 	response_type: 'code',
+// 	client_secret: YAHOO_API_SECRET,
+// 	debug: true
+// })
 
 export default class App extends React.Component {
+	componentDidMount(){
+		//client.callback();
+		Linking.addEventListener('url', url => {
+			console.log('url: ', url)
+		})
+	}
+
+	componentWillUnmount(){
+		Linking.removeEventListener('url', () => {
+			console.log('removed')
+		})
+	}
+
   render() {
     const store = createStore(reducers, {}, applyMiddleware(ReduxThunk))
     return (
--- src/actions/PlayerActions.js
+++ src/actions/PlayerActions.js
@@ -1,20 +1,23 @@
 import { AUTHORIZE } from './types';
 import axios from 'axios';
 import { YAHOO_API_ID, YAHOO_API_SECRET } from 'react-native-dotenv';
-import { JSO, Popup } from 'jso';
+// import { JSO, Popup } from 'jso';
 
-const CLIENT = new JSO({
-	providerID: 'yahoo',
-	client_id: YAHOO_API_ID,
-	redirect_uri: 'com.sixthman:/callback',
-	authorization: `https://api.login.yahoo.com/oauth2/request_auth?client_id=${YAHOO_API_ID}&redirect_uri=oob&response_type=code`,
-	response_type: 'code',
-	client_secret: YAHOO_API_SECRET,
-})
+// const CLIENT = new JSO({
+// 	providerID: 'yahoo',
+// 	client_id: YAHOO_API_ID,
+// 	redirect_uri: 'com.sixthman:/callback',
+// 	authorization: `https://api.login.yahoo.com/oauth2/request_auth?client_id=${YAHOO_API_ID}&redirect_uri=oob&response_type=code`,
+// 	response_type: 'code',
+// 	client_secret: YAHOO_API_SECRET,
+// })
 const ROOT_API_URL = 'https://fantasysports.yahooapis.com/fantasy/v2/';
 
 export const authorize = () => {
   return (dispatch) => {
-		CLIENT.callback()
+		axios.get(`https://api.login.yahoo.com/oauth2/request_auth?client_id=${YAHOO_API_ID}&redirect_uri=sixthman://app&response_type=code`)
+			.then(res => {
+				dispatch({ type: AUTHORIZE, payload: res.request.responseURL })
+			})
   }
 }
